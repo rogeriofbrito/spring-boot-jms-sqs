be32a56c05b3a42df94975d2fa822a16efd15722
Rogério <rogeriobritoufu@gmail.com>
2022-11-02 19:09:02 -0300

    first commit

---
+++ src/main/java/com/github/rogeriofbrito/springsqs/SpringSqsApplication.java
@@ -0,0 +1,13 @@
+package com.github.rogeriofbrito.springsqs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSqsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSqsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/rogeriofbrito/springsqs/SpringSqsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.rogeriofbrito.springsqs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSqsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
