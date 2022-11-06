459b1e0c1f746e6fb9c61eb0351cae8238f05621
Rogério <rogeriobritoufu@gmail.com>
2022-11-06 20:26:47 -0300

    adding dead letter queue

--- localstack.sh
+++ localstack.sh
@@ -1,1 +1,12 @@
-awslocal --region=us-east-1 sqs create-queue --queue-name product_queue
+awslocal \
+  --region=us-east-1 \
+  sqs create-queue \
+  --queue-name product_queue_dead_letter
+
+awslocal \
+  --region=us-east-1 \
+  sqs create-queue \
+  --queue-name product_queue \
+  --attributes '{
+                 "RedrivePolicy": "{\"deadLetterTargetArn\":\"arn:aws:sqs:us-east-1:000000000000:product_queue_dead_letter\",\"maxReceiveCount\":\"1000\"}"
+             }'
